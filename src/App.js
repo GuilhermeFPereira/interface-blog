91e4e45ea28fbcdd05b3bed253d07cc3f984e46e
GuilhermeFPereira <guilll.pereira@gmail.com>
2023-07-25 22:52:02 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Logo from '../src/assets/cra vs vite.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main>
+      <header>
+        <a href='' className='logo' >MyBlog</a>
+        <nav>
+          <a href='' >Login</a>
+          <a href=''>Register</a>
+        </nav>
       </header>
-    </div>
+      <div className='post' >
+        <div className='image' >
+        <img src={Logo} alt='' />
+        </div>
+        
+        <div className='texts'>
+        <h2>Full-house battery backup coming later this year</h2>
+        <p className='info'>
+          <a className='author' > Guilherme P.Fernandes </a>
+          <time>25-07-2023 22:42</time>
+        </p>
+        <p className='summary' >
+Olá tudo bom? Sou Desenvolvedor de software e estou aberto a novos desáfios.
+Caso esteja precisando de um Dev para seus projeto. Estou disposto a conversar sobre.
+Vou encaminhar meu currículo caso queira me conhecer melhor. Obrigado</p>
+        </div>
+       
+      </div>
+
+      <div className='post' >
+      <div className='image' >
+        <img src={Logo} alt='' />
+        </div>
+        
+        <div className='texts'>
+        <h2>Full-house battery backup coming later this year</h2>
+        <p className='info'>
+          <a className='author' > Guilherme P.Fernandes </a>
+          <time>25-07-2023 22:42</time>
+        </p>
+        <p className='summary' >
+Olá tudo bom? Sou Desenvolvedor de software e estou aberto a novos desáfios.
+Caso esteja precisando de um Dev para seus projeto. Estou disposto a conversar sobre.
+Vou encaminhar meu currículo caso queira me conhecer melhor. Obrigado</p>
+        </div>
+       
+      </div>
+
+      <div className='post' >
+      <div className='image' >
+        <img src={Logo} alt='' />
+        </div>
+        
+        <div className='texts'>
+        <h2>Full-house battery backup coming later this year</h2>
+        <p className='info'>
+          <a className='author' > Guilherme P.Fernandes </a>
+          <time>25-07-2023 22:42</time>
+        </p>
+        <p className='summary' >
+Olá tudo bom? Sou Desenvolvedor de software e estou aberto a novos desáfios.
+Caso esteja precisando de um Dev para seus projeto. Estou disposto a conversar sobre.
+Vou encaminhar meu currículo caso queira me conhecer melhor. Obrigado</p>
+        </div>
+       
+      </div>
+
+      
+    </main>
   );
 }
 
